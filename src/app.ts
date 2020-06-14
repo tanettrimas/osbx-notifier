ecd4d247a2bfee6ad9f71efa34313e8cc2575033
Tanet Trimas <tanet@trimas.no>
2020-06-14 12:39:57 +0200

    feat: 🎸 simple express entry point
    Added two files, the app and the index file for listening on ports

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
-export default (a: number, b: number): number => a + b;
+import express, { Request, Response, NextFunction } from 'express';
+import logger from 'morgan';
+
+import routes from './routes';
+
+const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api', routes);
+
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send({
+    message: 'Hello from /',
+  });
+});
+
+app.use('*', (req: Request, res: Response, next: NextFunction) => {
+  return next(new Error(`Route ${req.originalUrl} was not found!`));
+});
+
+app.use((error: Error, req: Request, res: Response) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  return res.send({
+    message: error.message,
+    stacktrace: process.env.NODE_ENV !== 'production' ? error.stack : null,
+  });
+});
+
+export default app;
