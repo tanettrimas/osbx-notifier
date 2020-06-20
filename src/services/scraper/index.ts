cf61c76635caeff47f47630a69ea6adf12a60ff4
Tanet Trimas <tanet@trimas.no>
2020-06-20 14:13:20 +0200

    feat: 🎸 added a scraper service
    This scraper is now configured with basic dependency injection but as
    for now, it is kinda using cheerio under the hood. In the future, this
    could be modularized even more

---
+++ src/services/scraper/index.ts
@@ -0,0 +1,38 @@
+import httpRequester from '../../lib/http';
+
+// TODO: Refactor CheerioElement to a type with only the props needed. E.g only data, name, tag and children
+export interface ScraperService {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  scrape: (rows: CheerioElement[], scrapeFunc: (rows: CheerioElement[]) => any) => any;
+  getHTML: (url: string) => Promise<CheerioStatic>;
+}
+
+function Scraper({ webscraper }: { webscraper: CheerioAPI }): ScraperService {
+  async function getHTML(url: string) {
+    try {
+      const response = await httpRequester.get({
+        url,
+        headers: {
+          'Content-Type': 'text/html',
+        },
+        method: 'GET',
+      });
+      const html = await webscraper.load(response.body);
+      return html;
+    } catch (e) {
+      throw new Error(e.message);
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function scrape(rows: CheerioElement[], scrapeFunc: (rows: CheerioElement[]) => any) {
+    return scrapeFunc(rows);
+  }
+
+  return Object.freeze({
+    getHTML,
+    scrape,
+  });
+}
+
+export default Scraper;
